773453742926fecf36d231a1cfa6cf0d90ae5a38
pwang33 <pwang33@illinois.edu>
2020-11-23 23:31:28 -0800

    added the sidebar to ui.R need to work on the callback functions

--- ui.R
+++ ui.R
@@ -13,29 +13,63 @@ shinyUI(
           skin = "blue",
           dashboardHeader(title = "Movie Recommender"),
           
-          dashboardSidebar(disable = TRUE),
+          # add sidebar to allow toggel between the two recommender systems
+          dashboardSidebar(
+            sidebarMenu(
+              menuItem("Recommender by Genre", tabName = "rec_genre", icon = icon("film")),
+              menuItem("Recommender by Rating", tabName = "rec_rate", icon = icon("star"))
+            )
+          ),
 
           dashboardBody(includeCSS("css/movies.css"),
-              fluidRow(
-                  box(width = 12, title = "Step 1: Rate as many movies as possible", status = "info", solidHeader = TRUE, collapsible = TRUE,
-                      div(class = "rateitems",
-                          uiOutput('ratings')
-                      )
-                  )
-                ),
-              fluidRow(
-                  useShinyjs(),
-                  box(
-                    width = 12, status = "info", solidHeader = TRUE,
-                    title = "Step 2: Discover movies you might like",
-                    br(),
-                    withBusyIndicatorUI(
-                      actionButton("btn", "Click here to get your recommendations", class = "btn-warning")
-                    ),
-                    br(),
-                    tableOutput("results")
-                  )
-               )
+              tabItems(
+                # recommender by genre
+                tabItem(tabName = 'rec_genre',
+                          fluidRow(
+                            box(width = 12, title = "Step 1: Select your favorite genre", status = "info", solidHeader = TRUE, collapsible = TRUE,
+                                div(class = "rateitems",
+                                    uiOutput('user_genre')
+                                )
+                            )
+                          ),
+                          fluidRow(
+                            useShinyjs(),
+                            box(
+                              width = 12, status = "info", solidHeader = TRUE,
+                              title = "Step 2: Discover movies you might like",
+                              br(),
+                              withBusyIndicatorUI(
+                                actionButton("btn", "Click here to get your recommendations", class = "btn-warning")
+                              ),
+                              br(),
+                              tableOutput("rec_genre_results")
+                            )
+                          )
+                        ),
+                # recommender by user ratings
+                tabItem(tabName = 'rec_rate',
+                        fluidRow(
+                          box(width = 12, title = "Step 1: Rate as many movies as possible", status = "info", solidHeader = TRUE, collapsible = TRUE,
+                              div(class = "rateitems",
+                                  uiOutput('ratings')
+                              )
+                          )
+                        ),
+                        fluidRow(
+                          useShinyjs(),
+                          box(
+                            width = 12, status = "info", solidHeader = TRUE,
+                            title = "Step 2: Discover movies you might like",
+                            br(),
+                            withBusyIndicatorUI(
+                              actionButton("btn", "Click here to get your recommendations", class = "btn-warning")
+                            ),
+                            br(),
+                            tableOutput("results")
+                          )
+                        )
+                )
+              )
           )
     )
 ) 
